f5dd8c497b1582ccee1942d4fc7112d48d4955c5
Sonya1488 <127775334+Sofiia-28@users.noreply.github.com>
2023-08-14 13:52:39 +0300

    Summary

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const ulEl = document.querySelector(".gallery");
+
+function createGallery() {
+  const markupLiEl = galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item"><a class="gallery__link" href="${preview}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></li>`
+    )
+    .join("");
+  ulEl.insertAdjacentHTML("beforeend", markupLiEl);
+
+  ulEl.addEventListener("click", zoomItem);
+}
+createGallery();
+
+function zoomItem(event) {
+  event.preventDefault();
+  for (const item of galleryItems) {
+    if (event.target.getAttribute("src") === item.preview) {
+      const instance = basicLightbox.create(`
+            <img src="${item.original}" width="800" height="600">
+        `);
+      instance.show();
+    }
+    instance.close();
+  }
+}
